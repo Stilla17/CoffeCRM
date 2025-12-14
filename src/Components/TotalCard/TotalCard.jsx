a74b1c619fa05c98d461a38e647141c8ff1aacde
Stilla <sunnatilla95@gmail.com>
2025-12-14 22:14:17 +0500

    totalCard added

---
+++ src/Components/TotalCard/TotalCard.jsx
@@ -0,0 +1,28 @@
+
+import React from 'react'
+
+const TotalCard = ({ title, total, description, children }) => {
+    return (
+        <div className="relative bg-white max-w-70 rounded-2xl border border-[#e0d7cf] shadow-sm p-5 overflow-hidden hover:shadow-md transition-all" >
+            <div className="absolute -top-8 -right-8 w-28 h-28 bg-[#f1f7f3] rounded-full" />
+            <div className="absolute top-8 right-10 z-10">
+                {
+                    children
+                }
+            </div>
+            <div className="relative z-10">
+                <p className="text-[#9b6e63] text-sm font-medium">
+                    {title}
+                </p>
+                <p className="text-[#3e2723] text-3xl font-bold my-2">
+                    {total}
+                </p>
+                <p className="text-[#9b6e63] text-sm mt-1">
+                    {description}
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default TotalCard
--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,9 @@
-import { FolderOpen, Plus } from 'lucide-react'
+import { FolderOpen, Plus, TrendingDown, TrendingUp } from 'lucide-react'
 import React from 'react'
 import Button from '../../Components/Button/Button'
 import Nav from '../../Components/Nav/Nav'
-
+import TotalCard from '../../Components/TotalCard/TotalCard'
+import { Package } from 'lucide-react'
 const Dashboard = () => {
   return (
     <>
@@ -14,6 +15,20 @@ const Dashboard = () => {
           <Plus size={18} />
         </Button>
       </Nav>
+      <div className='grid grid-cols-4 mt-8 gap-6'>
+        <TotalCard title={"Total Products"} total={"2"} description={"2 active"}>
+          <Package size={26} className="text-[#2d5f3f]" />
+        </TotalCard>
+        <TotalCard title={"Total Stock"} total={"213231.0"} description={"units in inventory"}>
+          <TrendingUp size={26} color='#3e2723' className="text-[#2d5f3f]" />
+        </TotalCard>
+        <TotalCard title={"Total Imported"} total={"21.0"} description={"units received"}>
+          <TrendingUp size={26} className="text-[#2d5f3f]" />
+        </TotalCard>
+        <TotalCard title={"Total Exported"} total={"213.0"} description={"units shipped"}>
+          <TrendingDown size={26} color='#8d6e63' className="text-[#2d5f3f]" />
+        </TotalCard>
+      </div>
     </>
   )
 }
